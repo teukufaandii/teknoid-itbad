f7c32faf239d27a03ffb780c4e19b65d39c33451
hendriktarigann01 <hendriktarigan25@gmail.com>
2024-07-20 21:13:03 +0700

    update pencarian surat masuk

--- Surat/ajax/searchSM.php
+++ Surat/ajax/searchSM.php
@@ -1,37 +1,24 @@
 <?php
 session_start(); // Mulai session sebelum mengakses variabel $_SESSION
 include '../koneksi.php';
-$output='';
+$output = '';
 
-if(isset($_POST['query'])){
-  $search=$_POST['query'];
-  if(isset($_SESSION['akses'])) {
-    $diteruskan_ke = $_SESSION['akses']; // Ambil nilai session akses dari pengguna
-    // Tambahkan placeholder % di depan dan belakang search untuk pencarian per huruf
-    $search = '%' . $search . '%';
-    $stmt=$koneksi->prepare("SELECT * FROM tb_surat_dis WHERE (kode_surat LIKE ? OR asal_surat LIKE ? OR perihal LIKE ? OR tanggal_surat LIKE ?) AND diteruskan_ke = ?");
-    $stmt->bind_param("sssss", $search, $search, $search, $search, $diteruskan_ke);
-  } else {
-    // Handle jika session akses tidak terdefinisi
-    exit("Session akses tidak terdefinisi.");
-  }
+if (isset($_POST['query'])) {
+  $search = $_POST['query'];
+  // Tambahkan placeholder % di depan dan belakang search untuk pencarian per huruf
+  $search = '%' . $search . '%';
+  $stmt = $koneksi->prepare("SELECT * FROM tb_surat_dis WHERE kode_surat LIKE ? OR asal_surat LIKE ? OR perihal LIKE ? OR tanggal_surat LIKE ?");
+  $stmt->bind_param("ssss", $search, $search, $search, $search);
+} else {
+  $stmt = $koneksi->prepare("SELECT * FROM tb_surat_dis");
 }
-else{
-  if(isset($_SESSION['akses'])) {
-    $diteruskan_ke = $_SESSION['akses']; // Ambil nilai session akses dari pengguna
-    $stmt=$koneksi->prepare("SELECT * FROM tb_surat_dis WHERE diteruskan_ke = ?");
-    $stmt->bind_param("s", $diteruskan_ke);
-  } else {
-    // Handle jika session akses tidak terdefinisi
-    exit("Session akses tidak terdefinisi.");
-  }
-}
+
 $stmt->execute();
-$result=$stmt->get_result();
+$result = $stmt->get_result();
 
-if(mysqli_num_rows($result) > 0) {
+if ($result->num_rows > 0) {
   $counter = 1;
-  $output .= '<table id="tablesk" class="tablesorter">';
+  $output .= '<table id="tablesm" class="tablesorter">';
   $output .= '<thead>
                   <tr>
                       <th style="min-width: 75px;">No <i class="fas fa-sort"></i></th>
@@ -43,38 +30,33 @@ if(mysqli_num_rows($result) > 0) {
                       <th>Aksi</th>
                   </tr>
               </thead>';
-  while($row = mysqli_fetch_array($result)) {
-      $output .= '<tbody>';
-      $output .= '<tr>';
-      $output .= '<td style="min-width: 75px;">'. $counter++ .'</td>';
-      $output .= '<td>'.$row['kode_surat'].'</td>';
-      $output .= '<td>'.$row['asal_surat'].'</td>';
-      $output .= '<td>'.$row['perihal'].'</td>';
-      $output .= '<td>'.$row['tanggal_surat'].'</td>';
-      $output .= '<td>';
-      if ($row['status_baca']) {
-          $output .= 'Sudah Disposisi';
-      } else {
-          $output .= 'Belum Disposisi';
-      }
-      if ($row['status_tolak'] == 1) {
-          $output .= ' - Ditolak';
-      } else {
-          $output .= '';
-      }
-      if ($row['status_selesai'] == 1) {
-          $output .= ' - Selesai';
-      } else {
-          $output .= '';
-      }
-      $output .= '</td>';
-      $output .= '<td><a href="disposisi.php?id='.$row['id_surat'].'">Lihat</a></td>';
-      $output .= '</tr>';
-      $output .= '</tbody>';
+  $output .= '<tbody>';
+  while ($row = $result->fetch_assoc()) {
+    $output .= '<tr>';
+    $output .= '<td style="min-width: 75px;">' . $counter++ . '</td>';
+    $output .= '<td>' . $row['kode_surat'] . '</td>';
+    $output .= '<td>' . $row['asal_surat'] . '</td>';
+    $output .= '<td>' . $row['perihal'] . '</td>';
+    $output .= '<td>' . $row['tanggal_surat'] . '</td>';
+    $output .= '<td>';
+    if ($row['status_baca']) {
+      $output .= 'Sudah Disposisi';
+    } else {
+      $output .= 'Belum Disposisi';
+    }
+    if ($row['status_tolak'] == 1) {
+      $output .= ' - Ditolak';
+    }
+    if ($row['status_selesai'] == 1) {
+      $output .= ' - Selesai';
+    }
+    $output .= '</td>';
+    $output .= '<td><a href="disposisi.php?id=' . $row['id_surat'] . '">Lihat</a></td>';
+    $output .= '</tr>';
   }
+  $output .= '</tbody>';
   $output .= '</table>';
   echo $output;
 } else {
-echo "<tr><td colspan='7'>0 results</td></tr>";
+  echo "<table><tr><td colspan='7'>0 results</td></tr></table>";
 }
-?>
